d76bdc40dfa91a9e0cb1ce1227e9f85b5cdb439b
ssavane26 <savaneprod@gmail.com>
2022-09-08 20:47:22 -0400

    added to api routes

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.get('/feedback', (req, res) =>
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT} `)
 );

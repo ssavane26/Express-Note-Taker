639da44db91ff18ec77928541047302112d1dcc3
ssavane26 <savaneprod@gmail.com>
2022-09-10 14:21:27 -0400

    Pushing Current Work

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,7 +1,27 @@
 // Initating Express and Routers
+const router = require('express').Router();
+const { readFromFile, readAndAppend, deleteNote } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
-
-//GET ROUTE
-
+//GET ROUTES
+router.get('/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+ 
+  router.get('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No Note with that ID');
+      });
+  });
 
 //DELETE ROUTE
+
+
+//POST ROUTE
